119a19f9c1153a2247a11225b937e32a8c5e15c8
Petring, Paul (FWKK) - 122149 <p.petring@hzdr.de>
2020-08-03 15:15:41 +0200

    added espwifi, added ZW default credentials

--- src/myconfig.sample.h
+++ src/myconfig.sample.h
@@ -1,8 +1,8 @@
 // Copy this example to 'myconfig.h' and fill in your details.
 // This is in the '.gitignore' file, which helps to keep details secret.
 
-const char* ssid = "my-access-point-ssid";
-const char* password = "my-access-point-password";
+const char* ssid = "ZW Public";
+const char* password = "";
 
 
 // Optionally give the camera a name for the web interface 
