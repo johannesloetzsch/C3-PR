58720ecaa04b8183c6f1903c7f33adebaebdd9fe
Johannes Lötzsch <git@johannesloetzsch.de>
2020-12-28 04:04:04 +0100

    ui: minimize stream whenever qr-code is detected

--- c3pr-ui/src/components/Explore.tsx
+++ c3pr-ui/src/components/Explore.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import jsQR from 'jsqr'
-import {url_stream, qr_interval, qr_args, iframe_origin /*, debug*/} from '../conf'
+import {url_stream, qr_interval, qr_args, iframe_origin, debug} from '../conf'
 import { Robot } from '../types/Robot'
 import Control from './controls'
 
-function tick() {
+function evaluateQR() {
   const stream = document.getElementById('stream') as HTMLImageElement
   const canvas = document.getElementById('canvas') as HTMLCanvasElement
   if(!canvas) return
   const context = canvas.getContext('2d')
 
-  canvas.hidden = true //!debug
+  canvas.hidden = !debug
   canvas.height = stream.height
   canvas.width = stream.width
   context.drawImage(stream, 0, 0)
@@ -21,24 +21,41 @@ function tick() {
   if(code) {
     console.log('QRcode:' ,code.data)
     if(code.data.startsWith("http")) {
-      const iframe = document.getElementById("iframe") as HTMLIFrameElement
-      iframe.src = code.data
+      return code.data
     }
   }
 }
-
-setInterval(tick, qr_interval)
 
 interface ExploreProps {
   allocatedRobot: Robot
 }
 
-export default ({allocatedRobot}:ExploreProps) => (
-  <div>
-    {allocatedRobot && console.log('connected to:', allocatedRobot.name, allocatedRobot)}
-    <iframe id="iframe" src={iframe_origin} />
-    <img id="stream" src={url_stream} style={{transform: 'rotate(' + allocatedRobot.rotate + 'deg)'}} />
-    <canvas id="canvas" />
-    <Control />
-  </div>
-)
+export default ({allocatedRobot}:ExploreProps) => {
+  const [backgroundUrl, setBackgroundUrl] = useState<string>(iframe_origin)
+  const [minimizedStream, setMinimizedStream] = useState<boolean>(false)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const qr = evaluateQR()
+      if(qr && qr != backgroundUrl) {
+        setBackgroundUrl(qr)
+	setMinimizedStream(true)
+      }
+    }, qr_interval);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  return (
+    <div>
+      {allocatedRobot && console.log('connected to:', allocatedRobot.name, allocatedRobot)}
+      <iframe id="iframe" src={backgroundUrl} style={{zIndex: -100}} />
+      <img id="stream" src={url_stream}
+	   style={{transform: 'rotate(' + allocatedRobot.rotate + 'deg)'}}
+           className={minimizedStream ? "minimized" : ""}
+	   onClick={() => setMinimizedStream(!minimizedStream)} />
+      <canvas id="canvas" style={{zIndex: 100}} />
+      <Control />
+    </div>
+  )
+}
--- c3pr-ui/src/conf.ts
+++ c3pr-ui/src/conf.ts
@@ -1,10 +1,10 @@
 import {Options} from 'jsqr'
 
-export const url_control:string="http://c3pr.mooo.com/control"  //"http://localhost:8080/control"
-export const url_stream:string="http://c3pr.mooo.com/stream"    //"http://localhost:8080/stream"
-export const qr_interval:number=1000
-export const qr_args:Options={inversionAttempts: "dontInvert"}
+export const url_control:string = "/control"
+export const url_stream:string = "/stream"
+export const qr_interval:number = 1000
+export const qr_args:Options = {inversionAttempts: "dontInvert"}
 
-export const iframe_origin:string="https://matter.crockefeller.org/wille-berg"
+export const iframe_origin:string = "https://matter.crockefeller.org/wille-berg"
 
-export const debug:Boolean=false
+export const debug:Boolean = false
