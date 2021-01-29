35b6211c98735e44130b606f69195306ebee04e4
Johannes Lötzsch <git@johannesloetzsch.de>
2021-01-29 13:01:47 +0100

    changed entrance page

--- c3pr-ui/src/conf.ts
+++ c3pr-ui/src/conf.ts
@@ -5,6 +5,6 @@ export const url_stream:string = "/stream"
 export const qr_interval:number = 1000
 export const qr_args:Options = {inversionAttempts: "dontInvert"}
 
-export const iframe_origin:string = "https://matter.crockefeller.org/wille-berg"
+export const iframe_origin:string = "https://matter.crockefeller.org/wille-berg/entrance"
 
 export const debug:Boolean = false
